d3f565bf75925491935238fac187bcdbf5c0a200
kaki <kaki@aa.com>
2021-03-17 22:20:03 +0900

    ippo

--- proj1/app/urls.py
+++ proj1/app/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
+# from django.contrib.staticfiles.urls import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('base/', views.ManageView.as_view(), name="manage"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+
 
 
 #     path('', views.BookView.as_view(), name="book"),
